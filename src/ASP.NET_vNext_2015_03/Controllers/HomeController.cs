abc1a81d6e521f2558a964b8f3fdc8cbd620913b
Maurice de Beijer <mauricedb@computer.org>
2015-03-03 19:46:25 +0100

    Basic MVC controller

---
+++ src/ASP.NET_vNext_2015_03/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace ASP.NET_vNext_2015_03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+                
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/ASP.NET_vNext_2015_03/Startup.cs
+++ src/ASP.NET_vNext_2015_03/Startup.cs
@@ -13,6 +13,7 @@ namespace ASP.NET_vNext_2015_03
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
@@ -34,6 +35,17 @@ namespace ASP.NET_vNext_2015_03
             app.UseErrorPage();
 
             //app.Use((ctx, next) => { throw new DivideByZeroException(); });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
 
             app.UseWelcomePage();
         }
