3218b6289b30eab38aada0e0ea29e006e18dbd92
Maurice de Beijer <mauricedb@computer.org>
2015-03-03 16:51:57 +0100

    Starter project

---
+++ src/ASP.NET_vNext_2015_03/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+
+namespace ASP.NET_vNext_2015_03
+{
+    public class Startup
+    {
+
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+        }
+    }
+}
